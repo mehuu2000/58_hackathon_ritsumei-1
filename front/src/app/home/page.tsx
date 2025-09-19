a0e1fdbb5a2ba43d0a372fe928c185bf847b83e6
mehuu2000 <mehuu2000@gmail.com>
2025-09-19 18:08:46 +0900

    投稿選択中に場所選択するようにアナウンス(一時保存)

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import WoodenNavigation from '@/components/WoodenNavigation';
 import NewsComponent from '@/components/NewsComponent';
+import PostModeMessage from '@/components/PostModeMessage';
 
 // Leafletはクライアントサイドでのみ動作するため、dynamic importを使用
 const MapContainer = dynamic(() => import('@/components/MapContainer'), {
@@ -53,6 +54,9 @@ export default function HomePage() {
         isExpanded={isNewsExpanded}
         setIsExpanded={setIsNewsExpanded}
       />
+      
+      {/* 投稿モード時の案内メッセージ */}
+      <PostModeMessage isVisible={isPostMode} />
     </main>
   );
 }
---
+++ front/src/components/PostModeMessage.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+interface PostModeMessageProps {
+  isVisible: boolean;
+}
+
+export default function PostModeMessage({ isVisible }: PostModeMessageProps) {
+  if (!isVisible) return null;
+
+  return (
+    <div className="fixed top-24 left-1/2 transform -translate-x-1/2 z-50 pointer-events-none">
+      <p className="text-gray-800 font-semibold text-2xl">
+        場所を選択してください
+      </p>
+    </div>
+  );
+}
