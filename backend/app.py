6460602c490b5a41c06d3154fdb7a918497599a2
mstka <mstka2525@outlook.jp>
2025-09-20 19:16:17 +0900

    profile_debug

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from backend.auth.login import router as login_router
 from backend.auth.sign_up import router as signup_router
 from backend.auth.account_auth import router as account_auth_router
+from backend.auth.profile import router as profile_router
 
 
 app = FastAPI(title="Backend API", version="0.1.0")
@@ -9,6 +10,7 @@ app = FastAPI(title="Backend API", version="0.1.0")
 app.include_router(signup_router)
 app.include_router(login_router)
 app.include_router(account_auth_router)
+app.include_router(profile_router)
 
 
 # for local run: uvicorn backend.app:app --reload
