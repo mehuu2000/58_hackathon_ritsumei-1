600e6b06c0812c5eb6a5ec1d89b0ab7283c40fa0
hasseeee <hasegawa.takuya1204@gmail.com>
2025-09-21 10:14:09 +0900

    refact

--- backend/app.py
+++ backend/app.py
@@ -7,16 +7,6 @@ from backend.news.news_router import router as news_router
 
 app = FastAPI(title="Backend API", version="0.1.0")
 
-@app.on_event("startup")
-def print_routes():
-    """サーバー起動時に、登録されている全ルートを出力する"""
-    print("="*80)
-    print(" " * 25, "REGISTERED API ROUTES")
-    print("="*80)
-    for route in app.routes:
-        print(f"Path: {route.path}, Methods: {route.methods}, Name: {route.name}")
-    print("="*80)
-
 app.include_router(signup_router)
 app.include_router(login_router)
 app.include_router(account_auth_router)
